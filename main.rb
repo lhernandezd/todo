d049be3431f04849f911681f1ade38888ee49a79
Luis Hernández <ldhdv95@gmail.com>
2018-06-14 20:45:40 -0500

    Add file create.erb and changes in index.erb

--- main.rb
+++ main.rb
@@ -1,6 +1,26 @@
 require 'sinatra'
 require './config'
+
+todos = []
 #Rutas
 get '/' do
+    @todos = todos
     erb :index
+end
+
+get '/todos' do
+    @todos = todos
+    erb :index
+end
+
+get '/todos/create' do
+    erb :create
+end
+
+post '/todos' do
+    todos.push({
+        "title": params[:title],
+        "description": params[:description]
+    })
+    redirect '/todos' #Vuelve a la pagina
 end
